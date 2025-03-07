20cc88dab836996c8372f8eab074bfe17f32d938
Agelai <127940853+Agelai@users.noreply.github.com>
2025-03-07 20:50:07 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const serverUrl = 'https://rich-dolls-grin.loca.lt'; // Локальный сервер https://test-bot-8nm4.onrender.com http://localhost:3000
+const serverUrl = 'https://fresh-papayas-pay.loca.lt'; // Локальный сервер https://test-bot-8nm4.onrender.com http://localhost:3000
 
 
 
